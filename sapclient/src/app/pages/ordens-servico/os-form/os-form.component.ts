153b151833369131fa77b94ab14e6aac18041751
basis <gusmaomicaely@gmail.com>
2020-09-25 10:16:50 -0300

    Validator PF

--- sapclient/src/app/pages/ordens-servico/os-form/os-form.component.ts
+++ sapclient/src/app/pages/ordens-servico/os-form/os-form.component.ts
@@ -107,6 +107,7 @@ export class OsFormComponent implements OnInit {
       pontosFuncao: [null
         , [
           Validators.required,
+          Validators.pattern('^[0-9]+?.?[0-9]{1,3}$'),
         ]],
       fabrica: [null
         , [
